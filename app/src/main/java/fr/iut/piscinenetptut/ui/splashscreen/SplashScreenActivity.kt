cf0399d11dd5009ae90c83eb1cae4f7497f878c6
Theo Bouteiller <theo.bouteiller@etu.univ-amu.fr>
2019-12-08 13:17:15 +0100

    Init project

---
+++ app/src/main/java/fr/iut/piscinenetptut/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package fr.iut.piscinenetptut.ui.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.iut.piscinenetptut.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
